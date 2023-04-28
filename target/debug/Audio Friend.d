7cbc0e3f5032cc9c9125f66b3b5639ca3022bc66
conner <csawchuk11@gmail.com>
2023-04-28 02:02:58 -0400

    add targets to gitignore

--- target/debug/Audio Friend.d
+++
@@ -1,1 +0,0 @@
-/Volumes/macOS\ NVMe/Xcode\ Projects/Projects/rust/Audio\ Friend/target/debug/Audio\ Friend: /Volumes/macOS\ NVMe/Xcode\ Projects/Projects/rust/Audio\ Friend/src/file_io.rs
--- target/debug/deps/audio_friend-26877fe73694f41c.d
+++
@@ -1,5 +0,0 @@
-/Volumes/macOS NVMe/Xcode Projects/Projects/rust/Audio Friend/target/debug/deps/audio_friend-26877fe73694f41c: src/file_io.rs
-
-/Volumes/macOS NVMe/Xcode Projects/Projects/rust/Audio Friend/target/debug/deps/audio_friend-26877fe73694f41c.d: src/file_io.rs
-
-src/file_io.rs:
